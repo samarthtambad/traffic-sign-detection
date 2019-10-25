de09be0d40089548403f04223f613835f5fe15dc
Samarth Tambad <tambad.samarth@gmail.com>
2019-10-25 16:28:36 -0400

    increased network size

--- model.py
+++ model.py
@@ -26,10 +26,10 @@ nclasses = 43 # GTSRB as 43 classes
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv1 = nn.Conv2d(3, 100, kernel_size=5)
+        self.conv2 = nn.Conv2d(100, 200, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(500, 50)
+        self.fc1 = nn.Linear(200 * 5 * 5, 50)
         self.fc2 = nn.Linear(50, nclasses)
 
         # Spatial transformer localization-network
@@ -67,7 +67,7 @@ class Net(nn.Module):
         x = self.stn(x)
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 500)
+        x = x.view(-1, 200 * 5 * 5)
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=True)
         x = self.fc2(x)
