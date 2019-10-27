8c6431e649be39ecd41a2b6af885d88a8a96427f
Samarth Tambad <tambad.samarth@gmail.com>
2019-10-26 20:57:45 -0400

    revert to smaller model

--- model.py
+++ model.py
@@ -4,35 +4,17 @@ import torch.nn.functional as F
 
 nclasses = 43 # GTSRB as 43 classes
 
-# class Net(nn.Module):
-#     def __init__(self):
-#         super(Net, self).__init__()
-#         self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
-#         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-#         self.conv2_drop = nn.Dropout2d()
-#         self.fc1 = nn.Linear(500, 50)
-#         self.fc2 = nn.Linear(50, nclasses)
-#
-#     def forward(self, x):
-#         x = F.relu(F.max_pool2d(self.conv1(x), 2))
-#         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-#         x = x.view(-1, 500)
-#         x = F.relu(self.fc1(x))
-#         x = F.dropout(x, training=self.training)
-#         x = self.fc2(x)
-#         return F.log_softmax(x, dim=1)
-
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 200, kernel_size=5)
-        self.conv2 = nn.Conv2d(200, 300, kernel_size=5)
-        self.conv3 = nn.Conv2d(300, 500, kernel_size=4)
+        self.conv1 = nn.Conv2d(3, 100, kernel_size=5)
+        self.conv2 = nn.Conv2d(100, 200, kernel_size=5)
+        self.conv3 = nn.Conv2d(200, 500, kernel_size=4)
         self.conv2_drop = nn.Dropout2d()
         self.conv3_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(500 * 1 * 1, 100)
-        self.fc2 = nn.Linear(100, nclasses)
+        self.fc1 = nn.Linear(500 * 1 * 1, 50)
+        self.fc2 = nn.Linear(50, nclasses)
 
         # Spatial transformer localization-network
         self.localization = nn.Sequential(
