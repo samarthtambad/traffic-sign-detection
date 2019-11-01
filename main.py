676c57c9edc7c21fd58c0f2dc66ccf8b44dba113
Samarth Tambad <tambad.samarth@gmail.com>
2019-10-31 20:12:56 -0400

    added adaptive_avg_pool2d

--- main.py
+++ main.py
@@ -46,12 +46,12 @@ train_loader = torch.utils.data.DataLoader(
     torch.utils.data.ConcatDataset([
         datasets.ImageFolder(args.data + '/train_images', transform=data_transforms),
         # datasets.ImageFolder(args.data + '/train_images', transform=data_resize_crop),
-        datasets.ImageFolder(args.data + '/train_images', transform=data_rotate),
+        # datasets.ImageFolder(args.data + '/train_images', transform=data_rotate),
         # datasets.ImageFolder(args.data + '/train_images', transform=data_hvflip),
         # datasets.ImageFolder(args.data + '/train_images', transform=data_hflip),
         # datasets.ImageFolder(args.data + '/train_images', transform=data_vflip),
-        datasets.ImageFolder(args.data + '/train_images', transform=data_color_jitter),
-        datasets.ImageFolder(args.data + '/train_images', transform=data_grayscale),
+        # datasets.ImageFolder(args.data + '/train_images', transform=data_color_jitter),
+        # datasets.ImageFolder(args.data + '/train_images', transform=data_grayscale),
     ]),
     batch_size=args.batch_size, shuffle=True, num_workers=1)
 
--- model.py
+++ model.py
@@ -67,8 +67,10 @@ class Net(nn.Module):
         x = F.leaky_relu(self.conv6(x))
         x = F.leaky_relu(self.conv7_drop(self.conv7(x)))
         x = F.leaky_relu(self.conv8(x))
+        x = F.leaky_relu(self.conv9(x))
         # x = x.view(-1, 512 * 1 * 1)
         # global averaging over 6 × 6 spatial dimensions
-        x = F.adaptive_avg_pool2d(1)
+        x = F.adaptive_avg_pool2d(x, (1, 1))
+        x = x.view(64, 43)
         return F.log_softmax(x, dim=1)
 
